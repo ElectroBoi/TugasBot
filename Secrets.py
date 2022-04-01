2df3a2b61020360a37f5083aef893f0e24c4bf41
ElectroBoi <64182319+ElectroBoi@users.noreply.github.com>
2022-04-01 18:35:30 +0700

    Update Secrets.py

--- Secrets.py
+++ Secrets.py
@@ -1,8 +1,8 @@
 from platform import release
 import config
 
-testing = "MzIzMzk1NTY3NjgwMjI1Mjgx.WT0Plw.xOzob-fJ0r_7TX5PaskenLfajGQ"
-TugasBot = "OTU3MTk2MTM0NDU4MTk2MDI4.Yj7QXg.cfH8X8fsldgrfIiKhjHx6g9_w4Q"
+testing = ""
+TugasBot = ""
 
 if config.test:
     token = testing
